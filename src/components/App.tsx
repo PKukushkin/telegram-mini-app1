8557a11f86675053c6ebaee6a77f4d624f8f5fb3
PavelKukushkin <pavelkukushkin@hotmail.com>
2025-11-11 22:09:46 +0100

    Second commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,26 @@
 import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
-import { useLaunchParams, useSignal, miniApp } from '@tma.js/sdk-react';
+import { useLaunchParams, useSignal, miniApp, viewport } from '@tma.js/sdk-react';
 import { AppRoot } from '@telegram-apps/telegram-ui';
+import { useEffect } from 'react';
 
 import { routes } from '@/navigation/routes.tsx';
 
 export function App() {
   const lp = useLaunchParams();
   const isDark = useSignal(miniApp.isDark);
+
+  useEffect(() => {
+    async function setupFullscreen() {
+      if (viewport.mount.isAvailable()) {
+        await viewport.mount();
+        viewport.expand();
+        if (viewport.requestFullscreen.isAvailable()) {
+          await viewport.requestFullscreen();
+        }
+      }
+    }
+    setupFullscreen();
+  }, []);
 
   return (
     <AppRoot
